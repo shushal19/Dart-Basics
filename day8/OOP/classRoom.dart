3813caadcc15862a4354ca5d020d85b6b65a03dc
Shusal Lamsal <shusallamsal35@gmail.com>
2023-10-05 12:28:29 +0545

    oop classroom

---
+++ day8/OOP/classRoom.dart
@@ -0,0 +1,12 @@
+class DesVuOffice{
+int? Nolaptops=5;
+String? bestStudent="Shusal lamsal";
+ }
+void main()
+{
+  DesVuOffice d1=DesVuOffice();
+  print("NUmber of laptop:");
+  print(d1.Nolaptops);
+  print("best student:${d1.bestStudent}");
+
+}
